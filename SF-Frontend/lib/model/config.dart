ed5a37f2e07b2078b5ce76fd2e7bfd388539a9c6
Isuru49 <114619691+Isuru49@users.noreply.github.com>
2024-06-08 01:14:54 +0530

    updated

--- SF-Frontend/lib/model/config.dart
+++ SF-Frontend/lib/model/config.dart
@@ -1,4 +1,4 @@
-const url = 'http://10.11.18.99:3000/';
+const url = 'http://192.168.187.143:3000/';
 const registration = "${url}registration";
 const login = '${url}login';
 const rcrops = '${url}RecommendedCrops';
